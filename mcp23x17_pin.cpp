8664976fa8b8c4e1e9bf70d795f43ca36117ec11
Djuri Baars <dsbaars@users.noreply.github.com>
2023-12-10 23:08:26 +0100

    Add mutex

--- mcp23x17_pin.cpp
+++ mcp23x17_pin.cpp
@@ -1,5 +1,7 @@
 #include "mcp23x17_pin.hpp"
 
+std::mutex MCP23X17_Pin::mcpMutex;
+
 MCP23X17_Pin::MCP23X17_Pin(Adafruit_MCP23XXX& mcp, uint pinNumber) : mcp(mcp), UniversalPin{pinNumber} {
     this->mcp = mcp;
 }
@@ -10,10 +12,14 @@ void MCP23X17_Pin::pinMode(uint8_t mode)
 }
 
 uint8_t MCP23X17_Pin::digitalRead() {
+    std::lock_guard<std::mutex> lockMcp(MCP23X17_Pin::mcpMutex);
+
     uint8_t res = this->mcp.digitalRead(this->pinNumber);
     return res;
 }
 
 void MCP23X17_Pin::digitalWrite(uint8_t value) {
+    std::lock_guard<std::mutex> lockMcp(MCP23X17_Pin::mcpMutex);
+
     this->mcp.digitalWrite(this->pinNumber, value);
 }
--- mcp23x17_pin.hpp
+++ mcp23x17_pin.hpp
@@ -1,9 +1,12 @@
 #include "universal_pin.hpp"
 #include <Adafruit_MCP23X17.h>
+#include <mutex>
 
 class MCP23X17_Pin : public UniversalPin
 {
 public:
+    static std::mutex mcpMutex;
+
     MCP23X17_Pin(Adafruit_MCP23XXX& mcp, uint pinNumber);
     void pinMode(uint8_t mode);
     uint8_t digitalRead();
@@ -12,3 +15,4 @@ public:
 protected:
     Adafruit_MCP23XXX& mcp;
 };
+
